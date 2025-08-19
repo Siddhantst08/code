0b083d57ffcf4f2dc2632373a1e3f67c85ed2459
Siddhant Tiwari <76822423+Siddhantst08@users.noreply.github.com>
2025-08-19 12:55:17 +0530

    Update generative-ai-chatbot-history.component.ts

--- generative-ai-chatbot-history.component.ts
+++ generative-ai-chatbot-history.component.ts
@@ -1,14 +1,5 @@
-
-class UsableFlowWithCreatorName(BaseModel):
-    id: str
-    flowId: str
-    templateName: str
-    description: Optional[str]
-    creator: str
-    updater: str
-    executionCount: int
-    categoryNames: List[str]
-    toolNames: List[str]
-    status: str
-    isActive: bool
-    isDeleted: bool
+class Counts(BaseModel):
+    publishedCount: int
+    inDevelopmentCount: int
+    activeCount: int
+    inActiveCount: int
